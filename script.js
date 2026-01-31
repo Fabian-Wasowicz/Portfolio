eaab83ff77d6fad93bb56d9aa70df5435714c3c5
Fanta <fabian.wasowicz.383@accesscreative.ac.uk>
2026-01-31 18:29:09 +0000

    31/01/2026
    I have finished the main body of the portfolio, fixed the JavaScript bug (I just typed the directory wrong) and replaced all of the lorem text. Just need to add links to all of the buttons e.g. a button to take you to more information about my projects.

--- script.js
+++ script.js
@@ -22,11 +22,27 @@ window.onscroll = () => {
             });
         };
     });
+    /*Sticky navbar*/
 
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
+    /*Remove toggle*/
+
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+
+/*Scroll reveal*/
+ScrollReveal({ 
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
